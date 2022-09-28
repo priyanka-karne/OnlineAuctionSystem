0c821b34ef9f2a7944de9e7daa831afbb07568af
priyanka karne <priyankakarne2017@gmail.com>
2022-09-28 16:44:13 +0530

    Uploaded Successfully

---
+++ Backend/src/main/java/com/app/config/WebSoket.java
@@ -0,0 +1,32 @@
+package com.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+@Configuration
+
+@EnableWebSocketMessageBroker
+public class WebSoket implements WebSocketMessageBrokerConfigurer{
+	
+	
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		//where web socket is created
+		WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
+		registry.addEndpoint("/rt-auction").setAllowedOrigins("*").withSockJS();
+	}
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		// TODO Auto-generated method stub
+		WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
+		registry.enableSimpleBroker("/rt-product");
+        registry.setApplicationDestinationPrefixes("/app");
+	}
+	
+	
+
+}
